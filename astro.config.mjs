e3cc6449d752c20d191999da837c255d6b8976ee
cyou <cn.youc@gmail.com>
2025-08-01 23:50:10 +0800

    chore: publish new blog post and update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://blog.zsdy.dev/",
   base: "/",
   trailingSlash: "always",
 
